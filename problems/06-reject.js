974b5b1e685bc6ec5706aeb3b4c5b0f0f1e0b23a
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-02 00:14:39 -0400

    solved

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -22,8 +22,27 @@ console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth'
 *******************************************************************************/
 
 function reject(array, cb) {
-  // Your code here 
+  // Your code here
+  let rejected = [];
+  for (let element of array) {
+    if (!cb(element)){
+      rejected.push(element);
+    }
+  }
+  return rejected;
 }
+
+//Examples
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
+
+let hasA = function(s) {
+  return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
+
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
