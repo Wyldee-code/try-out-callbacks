4b95349f6f0e23258479b16e511254c00e50d475
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-01 23:21:51 -0400

    solved 3

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -22,8 +22,28 @@ console.log(result3); // hi!!!!!
 *******************************************************************************/
 
 function multiMap(val, n, cb) {
-  // Your code here 
+  // Your code here
+  let newResult = val;
+  for (let num of Array(n)) {
+    newResult = cb(newResult);
+  }
+return newResult;
 }
+
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
+
+let result2 = multiMap(7, 3, function(n) {
+    return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
