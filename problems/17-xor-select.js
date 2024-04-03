9c1d3df7f9997719bac357fbbd13bcd7e0d8e17b
Wyldeliz <santoswyldeliz@gmail.com>
2024-04-03 00:17:15 -0400

    solved

--- problems/17-xor-select.js
+++ problems/17-xor-select.js
@@ -32,9 +32,41 @@ console.log(
 *******************************************************************************/
 
 let xorSelect = function(array, cb1, cb2) {
-  // Your code here 
+  let result = [];
+  // Iterate through each element of the arry
+for (let element of array) {
+  // check if the element passed in one of the callbacks, but not both.
+  if ((cb1(element) && !cb2(element)) || (!cb1(element) && cb2(element))){
+    result.push(element);
+  }
+}
+return result
 };
 
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+
+let isPositive = function(n) {
+  return n > 0;
+};
+
+console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+// [ -2, 1, 3 ]
+
+
+let longString = function(s) {
+  return s.length > 4;
+};
+
+let startsA = function(s) {
+  return s[0] === "a";
+};
+
+console.log(
+  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+);
+// [ 'art', 'app', 'buttery' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = xorSelect;
